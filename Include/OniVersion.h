3e33ce60c84956cc83a675c11fe26cd9a46d5853
oz.magal <oz.magal@primesense.com>
2012-12-10 10:28:29 +0200

    Version 2.0.0.30
    Signed-off-by: eddie.cohen <eddie.cohen@primesense.com>

--- Include/OniVersion.h
+++ Include/OniVersion.h
@@ -23,7 +23,7 @@
 #define ONI_VERSION_MAJOR	2
 #define ONI_VERSION_MINOR	0
 #define ONI_VERSION_MAINTENANCE	0
-#define ONI_VERSION_BUILD	29
+#define ONI_VERSION_BUILD	30
 
 /** OpenNI version (in brief string format): "Major.Minor.Maintenance (Build)" */ 
 #define ONI_BRIEF_VERSION_STRING \
--- Redist/UpdateVersion.py
+++ Redist/UpdateVersion.py
@@ -28,7 +28,7 @@ from datetime import date
 VERSION_MAJOR = 2
 VERSION_MINOR = 0
 VERSION_MAINTENANCE = 0
-VERSION_BUILD = 29
+VERSION_BUILD = 30
 
 class UpdateVersion:
     def main(self):
