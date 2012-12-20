2a33fa537ea9ba234ba0c2993562084bf69b4dcd
Ziv Hendel <ziv@primesense.com>
2012-12-20 19:39:43 +0200

    Added MacOSX Support
    Signed-off-by: eddie.cohen <eddie.cohen@primesense.com>

---
+++ ThirdParty/PSCommon/XnLib/Include/MacOSX/XnOSMacOSX.h
@@ -0,0 +1,30 @@
+/*****************************************************************************
+*                                                                            *
+*  PrimeSense PSCommon Library                                               *
+*  Copyright (C) 2012 PrimeSense Ltd.                                        *
+*                                                                            *
+*  This file is part of PSCommon.                                            *
+*                                                                            *
+*  Licensed under the Apache License, Version 2.0 (the "License");           *
+*  you may not use this file except in compliance with the License.          *
+*  You may obtain a copy of the License at                                   *
+*                                                                            *
+*      http://www.apache.org/licenses/LICENSE-2.0                            *
+*                                                                            *
+*  Unless required by applicable law or agreed to in writing, software       *
+*  distributed under the License is distributed on an "AS IS" BASIS,         *
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+*  See the License for the specific language governing permissions and       *
+*  limitations under the License.                                            *
+*                                                                            *
+*****************************************************************************/
+#ifndef _XN_OSMACOSX_H_
+#define _XN_OSMACOSX_H_
+
+// Start with Linux-x86, and override what's different
+#include "../Linux-x86/XnOSLinux-x86.h"
+
+#undef XN_SHARED_LIBRARY_POSTFIX
+#define XN_SHARED_LIBRARY_POSTFIX "*.dylib"
+ 
+#endif //_XN_OSMACOSX_H_
