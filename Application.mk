ef6f26cc369b53e2395bee6b42384ca5fb34beb0
Ziv Hendel <ziv@primesense.com>
2013-02-05 18:02:59 +0200

    Rewrote the android make files to proper standards. They can now also support both OS and NDK builds.

--- Application.mk
+++ Application.mk
@@ -1,20 +1,25 @@
-# Android >= v2.3
+# OpenNI 2.x Android makefile. 
+# Copyright (C) 2012 PrimeSense Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License. 
+
+# *** 
+# *** Note: This module file is only in use when building via NDK! ***
+# *** 
+
+# Android should be >= v2.3
 APP_PLATFORM := android-9
 
-# Build ARMv7-A machine code.
+# Use ARM v7a instruction set
 APP_ABI := armeabi-v7a
-APP_CFLAGS := -O3 -ftree-vectorize -ffast-math -funroll-loops
-
-APP_CFLAGS += -fPIC
-
-ifeq ($(APP_ABI),armeabi-v7a)
-	APP_CFLAGS += -march=armv7-a -mfloat-abi=softfp -mtune=cortex-a9 -mfp=vfpv3-d16 -mfpu=vfp
-
-# optionally add NEON to compilation flags.
-# to activate, run: "ndk-build USE_NEON=1"
-ifdef USE_NEON
-$(call __ndk_info,Building everything with NEON support!)
-	APP_CFLAGS += -mfpu=neon -DHAVE_NEON=1 -flax-vector-conversions
-endif
-endif
-
+ARCH_ARM_HAVE_ARMV7A := true
