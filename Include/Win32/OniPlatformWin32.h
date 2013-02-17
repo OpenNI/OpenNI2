1e5dc11fb88408dd41fea3f5887679a96b0fe97c
Eddie Cohen <eddie.cohen@primesense.com>
2013-02-17 11:29:54 +0200

    Add additional stdint types to support VS2008

--- Include/Win32/OniPlatformWin32.h
+++ Include/Win32/OniPlatformWin32.h
@@ -55,8 +55,16 @@
 #include <assert.h>
 #include <float.h>
 #include <crtdbg.h>
+
 #if _MSC_VER < 1600 // Visual Studio 2008 and older doesn't have stdint.h...
+typedef signed char int8_t;
+typedef short int16_t;
+typedef int int32_t;
 typedef __int64 int64_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
 typedef unsigned __int64 uint64_t;
 #else
 #include <stdint.h>
