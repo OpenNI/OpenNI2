cc226bcc3b3711fcd20ad23ea17f704930fb6f4f
Eddie Cohen <eddie.cohen@primesense.com>
2013-07-16 12:27:48 +0300

    Update version to 2.3.0.1

--- Include/OniVersion.h
+++ Include/OniVersion.h
@@ -21,9 +21,9 @@
 #include "OniPlatform.h"
 
 #define ONI_VERSION_MAJOR	2
-#define ONI_VERSION_MINOR	2
+#define ONI_VERSION_MINOR	3
 #define ONI_VERSION_MAINTENANCE	0
-#define ONI_VERSION_BUILD	30
+#define ONI_VERSION_BUILD	1
 
 /** OpenNI version (in brief string format): "Major.Minor.Maintenance (Build)" */ 
 #define ONI_BRIEF_VERSION_STRING \
