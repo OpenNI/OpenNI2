f8b1193205af605439ecc73a7d6959b213f55587
Eddie Cohen <eddie.cohen@primesense.com>
2013-09-23 17:45:18 +0300

    Update version to 2.3.0.7

--- Include/OniVersion.h
+++ Include/OniVersion.h
@@ -23,7 +23,7 @@
 #define ONI_VERSION_MAJOR	2
 #define ONI_VERSION_MINOR	3
 #define ONI_VERSION_MAINTENANCE	0
-#define ONI_VERSION_BUILD	6
+#define ONI_VERSION_BUILD	7
 
 /** OpenNI version (in brief string format): "Major.Minor.Maintenance (Build)" */ 
 #define ONI_BRIEF_VERSION_STRING \
--- Packaging/UpdateVersion.py
+++ Packaging/UpdateVersion.py
@@ -28,7 +28,7 @@ from datetime import date
 VERSION_MAJOR = 2
 VERSION_MINOR = 3
 VERSION_MAINTENANCE = 0
-VERSION_BUILD = 6
+VERSION_BUILD = 7
 
 def getVersionString():
     return str(VERSION_MAJOR) + "." + str(VERSION_MINOR) + "." + str(VERSION_MAINTENANCE) + "." + str(VERSION_BUILD)
