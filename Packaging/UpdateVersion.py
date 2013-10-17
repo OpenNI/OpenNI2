4588ef1b6db34d479ce1553cfd0bf624d7c13890
Eddie Cohen <eddie.cohen@primesense.com>
2013-10-17 16:11:07 +0300

    Update version to 2.3.0.11

--- Packaging/UpdateVersion.py
+++ Packaging/UpdateVersion.py
@@ -28,7 +28,7 @@ from datetime import date
 VERSION_MAJOR = 2
 VERSION_MINOR = 3
 VERSION_MAINTENANCE = 0
-VERSION_BUILD = 10
+VERSION_BUILD = 11
 
 def getVersionString():
     return str(VERSION_MAJOR) + "." + str(VERSION_MINOR) + "." + str(VERSION_MAINTENANCE) + "." + str(VERSION_BUILD)
